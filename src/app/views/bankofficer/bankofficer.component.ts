486beebf7c4436b1ef647b5a37f97812932ad998
01Junel <52649280+01Junel@users.noreply.github.com>
2023-10-20 18:20:45 +0800

    Added the import for NgZone, ChangeDetectionStrategy, ChangeDetectorRef. Change the way the the function for fetching the Data of the Officers so that instead of the Company CIS number, The Company Name which the Officers are related to will be the one displaying in the DataTables. Added the SetButton to set the Id for the Relationship and the Officers CIS number to link the Related Interest to the proper Officers. And removed some codes that are commented and that are not going to be use anymore.

--- src/app/views/bankofficer/bankofficer.component.ts
+++ src/app/views/bankofficer/bankofficer.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, NgZone, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -14,7 +14,7 @@ import { DataTransferService } from '../../services/data-transfer.service';
 // Functions Import
 import {createBankOfficer} from '../../functions-files/addBankOfficer';
 import {createBankOfficerRelationship} from '../../functions-files/addBankOfficerRelationship';
-import {getOfficers, getOfficersRI} from '../../functions-files/getFunctions'
+import {getOfficers, getCompany, getOfficersRI} from '../../functions-files/getFunctions'
 
 export interface Child {
   name: string;
@@ -27,15 +27,40 @@ export interface Data {
   mothersname: String,
   fathersname: String,
   spouse: String,
-  children?: Child[];
+  children: String;
   motherinlaw: String,
   fatherinlaw: String,
+}
+
+interface RelatedInterest {
+  id: number;
+  cis_number: string;
+  fname: string;
+  mname: string;
+  lname: string;
+  dir_related: string;
+  relation: number;
+  date_inserted: Date;
+  status: number;
+}
+
+// Interface for the director item
+interface Officers {
+  // id: number;
+  // com_related: string;
+  cis_number: string;
+  fname: string;
+  mname: string;
+  lname: string;
+  com_cisnumber: string;
+  // related_interest: RelatedInterest[];
 }
 
 @Component({
   selector: 'app-bankofficer',
   templateUrl: './bankofficer.component.html',
-  styleUrls: ['./bankofficer.component.scss']
+  styleUrls: ['./bankofficer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 @Injectable({
@@ -49,10 +74,16 @@ export class BankofficerComponent implements AfterViewInit {
   boForm: FormGroup;
   boRIForm: FormGroup;
   buttonId: number = 0;
+  selectedcomCisNumber: number = 0;
+  Company: any;
+  CompData: any;
+  CompName: any;
+  companies:  any = [];
+  tableData: Record<string, any>[] = [];
 
-  displayedColumns: string[] = ['fname', 'com_related', 'position', 'mothersname', 'fathersname', 'spouse',
-  'children', 'motherinlaw', 'fatherinlaw'];
-  dataSource = new MatTableDataSource<Data>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = ['FullName', 'Company', 'Position', "MothersName", "FathersName", 'Spouse', 'Children', 'MotherinLaw', 'FatherinLaw'];
+  // dataSource = new MatTableDataSource<Data>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -67,7 +98,9 @@ export class BankofficerComponent implements AfterViewInit {
   constructor(private router: Router,
           private formBuilder: FormBuilder, 
           private http: HttpClient, 
-          private dataTransferService: DataTransferService)
+          private dataTransferService: DataTransferService,
+          private changeDetectorRef: ChangeDetectorRef,
+          private ngZone: NgZone)
           {
             this.boForm = this.formBuilder.group({
               boCisNumber: [''],
@@ -85,25 +118,63 @@ export class BankofficerComponent implements AfterViewInit {
     }
 
   ngOnInit(): void {
-    getOfficers((Data) => {
+    this.updateTableData();
+   }
+
+
+
+  // Functions Below
+  updateTableData(): void {
+    getCompany((compData) => {
+      console.log(compData);
       // Process the data to count directors related to each company
-        const companiesWithDirectors = Data.map(company => {
-          const directors = company.directors || []; // Ensure there is a directors array
-          const directorCount = directors.length;
-          return { ...company, directorCount, directors };
-        });
+      getOfficers((Officers) => {
+        console.log(Officers);
+        
+        const relationColumn = ['MothersName', 'FathersName', 'Spouse', 'Children', 'MotherinLaw', 'FatherinLaw'];
+        const tableData: Record<string, any>[] = [];
+    
+        for (const officer of Officers) {
+          const officerData = officer.Officers || [];
+    
+          // Find the company that matches the officer's com_related
+          const matchingCompany = compData.find((company) => company.com_cis_number === officer.com_related);
+          const companyName = matchingCompany ? matchingCompany.com_company_name : '';
+    
+          const row: Record<string, any> = {
+            'FullName': `${officer.fname} ${officer.mname}  ${officer.lname}`,
+            'Company': companyName,
+            'Position': officer.position,
+            'offc_CisNumber': officer.off_cisnumber,
+          };
+          tableData.push(row);
+        }
+        
+        this.dataSource.data = tableData;
+        // Trigger change detection
+        this.changeDetectorRef.detectChanges();
+      });
 
-        // Set the data source for your MatTable
-        this.dataSource.data = companiesWithDirectors;
+     
     });
-    getOfficersRI()
+    
+
+
   }
 
-  // Functions
+  
 
-  setButtonId(id: number) {
+
+  setButtonId(id: number, comCisNumber: number) {
     this.buttonId = id;
+    this.selectedcomCisNumber = comCisNumber;
+    console.log(comCisNumber);
+    console.log(id);
+    
   }
+  // setButtonId(id: number) {
+  //   this.buttonId = id;
+  // }
 
   onBOSubmit() {
  
@@ -113,6 +184,16 @@ export class BankofficerComponent implements AfterViewInit {
       // Call the JavaScript function with form data
       createBankOfficer(boData); // Pass the entire formData object
     }
+
+
+    this.ngZone.run(() => {
+      this.dataSource.data = this.tableData;
+    });
+
+      // Trigger change detection
+    this.changeDetectorRef.detectChanges();
+    console.log(this.changeDetectorRef.detectChanges);
+    console.log(this.dataSource);
   }
 
   onBORISubmit() {
@@ -121,7 +202,7 @@ export class BankofficerComponent implements AfterViewInit {
       const boRIData = this.boRIForm.value;
   
       // Call the JavaScript function with form data
-      createBankOfficerRelationship(boRIData); // Pass the entire formData object
+      createBankOfficerRelationship(boRIData, this.buttonId, this.selectedcomCisNumber); // Pass the entire formData object
     }
   }
 
@@ -131,6 +212,16 @@ export class BankofficerComponent implements AfterViewInit {
     
   }
 
+
+  delRelationship() {
+    // deleteRelationship()
+  }
+
+
+  setoffcRelated() {
+    // director = director.dir_related;
+    // console.log(director);
+  }
   // addBankOfficer() {
   //   createBankOfficer()
   // }
@@ -142,22 +233,3 @@ export class BankofficerComponent implements AfterViewInit {
 }
 
 
-// Data Sets
-const ELEMENT_DATA: Data[] = [
-  {
-    fullname: "John Doe",
-    company: 'All Bank',
-    position: 'Director',
-    mothersname: 'sample',
-    fathersname: 'sample',
-    spouse: 'sample',
-    children: [
-      { name: 'CAMACHO, JAVIER FAUSTO' },
-      { name: 'CAMACHO, GERARDO FAUSTO' },
-      {name: 'CAMACHO, ELIRITA FAUSTO'},
-      {name: 'CAMACHO, REGINA FAUSTO'},
-    ],
-    motherinlaw: 'sample',
-    fatherinlaw: 'sample',
-  },
-];
