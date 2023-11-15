79c7a2d1d2a6562e63e40b36f100182e51c7c8d4
01Junel <52649280+01Junel@users.noreply.github.com>
2023-11-15 09:13:33 +0800

    Added the MaintenanceRoutingModule for the Users, Settings, and Manuals routing

--- src/app/views/maintenance/maintenance.module.ts
+++ src/app/views/maintenance/maintenance.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-// import { MaintenanceRoutingModule } from './maintenance-routing.module';
+import { MaintenanceRoutingModule } from './maintenance-routing.module';
+import { ManualsComponent } from './manuals/manuals.component';
 
 @NgModule({
   declarations: [
+  
+    ManualsComponent
   ],
   imports: [
     CommonModule,
-    // MaintenanceRoutingModule
+    MaintenanceRoutingModule
   ]
 })
 export class MaintenanceModule { }
