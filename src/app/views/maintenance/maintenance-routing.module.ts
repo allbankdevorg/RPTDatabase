9be828ffe8604dfa7f0c013846d96e56dffac1f6
01Junel <52649280+01Junel@users.noreply.github.com>
2023-11-15 09:09:26 +0800

    change the path for the user, settings. and added the path for the manuals

--- src/app/views/maintenance/maintenance-routing.module.ts
+++ src/app/views/maintenance/maintenance-routing.module.ts
@@ -3,26 +3,37 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { UsersComponent } from './users/users.component';
 import { SettingsComponent } from './settings/settings.component';
+import { ManualsComponent } from './manuals/manuals.component';
 
 const routes: Routes = [
-    {
+        {
           path: '',
-          pathMatch: 'full',
-          redirectTo: 'users',
-        },
-        {
-          path: './users',
-          component: UsersComponent,
           data: {
-            title: 'Users',
+            title: 'Maintenance',
           },
-        },
-        {
-            path: './settings',
-            component: SettingsComponent,
-            data: {
-                title: 'Settings',
+          children: [
+            {
+              path: 'users',
+              component: UsersComponent,
+              data: {
+                title: 'Users ',
+              },
             },
+            {
+              path: 'settings',
+              component: SettingsComponent,
+              data: {
+                title: 'Settings ',
+              },
+            },
+            {
+              path: 'manuals',
+              component: ManualsComponent,
+              data: {
+                title: 'Manuals ',
+              },
+            },
+          ]
         },
   ];
 
