8c1f245f860c9ce68ef7bf364bdfb81f6d91b025
01Junel <52649280+01Junel@users.noreply.github.com>
2023-11-17 17:51:39 +0800

    Created the other-related-companies components and module. Then added the table data for the tree diagram of the companies under JMN. then added the css for the modal and the pop up. Then fix the issue for the delay loading of the Tree

---
+++ src/app/views/other-related-companies/other-related-companies.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { otherRelatedCompaniesRoutingModule } from './other-related-companies-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    otherRelatedCompaniesRoutingModule
+  ]
+})
+export class OtherRelatedCompaniesModule { }
