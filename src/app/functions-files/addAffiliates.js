ae95ecccbadba72b2aa2dbe7c74090a43b7051fd
01Junel <52649280+01Junel@users.noreply.github.com>
2023-11-17 17:34:48 +0800

    Change the fucntion into promise to get the response message in the typescript code

--- src/app/functions-files/addAffiliates.js
+++ src/app/functions-files/addAffiliates.js
@@ -1,28 +1,48 @@
-function createAffil(formData) {
+function createAffil(formData, moduleV) {
+  return new Promise((resolve, reject) => {
     console.log(formData)
-   var settings = {
-     "url": "http://10.232.236.15:8092/api/addData",
-     "method": "POST",
-     "timeout": 0,
-     "headers": {
-       "Content-Type": "application/json"
-     },
-     "data": JSON.stringify({
-       "cmd": 6,
-       "request": {
-             "cis_number": formData.affilCisNumberM,       // Use form data
-             "account_name": formData.accountName,   // Use form data
-             "company_name": formData.companyName,
-             "manager": formData.commandControl,  // Use form data
-           }
-     }),
-   };
+    console.log(moduleV);
+    var settings = {
+      "url": "http://10.232.236.15:8092/api/addData",
+      "method": "POST",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": JSON.stringify({
+        "cmd": 6,
+        "request": {
+          "cis_number": formData.affilCisNumberM,
+          "account_name": formData.accountName,
+          "company_name": formData.companyName,
+          "manager": formData.commandControl,
+          "module": moduleV,
+        }
+      }),
+    };
+
+    $.ajax(settings).done(function (response) {
+      // Log the response
+      console.log(response.result[0].status);
+      
+      // Check the status and resolve/reject the promise accordingly
+      Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
+      if (response.result[0].status === 'success') {
+        resolve(response);
+      } else {
+        reject(response);
+      }
+    });
+  });
+   
+  //  $.ajax(settings).done(function (response) {
+  //    Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
+  //    console.log(response.result[0].status);
+  //    return response;
+  //   //  console.log(response);
+  //  });
 
    
-   $.ajax(settings).done(function (response) {
-     Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
-     console.log(response);
-   });
 
    
 
