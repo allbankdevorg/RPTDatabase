4efbc905c452f7adb6340f983a6cbf19b5aadba3
01Junel <52649280+01Junel@users.noreply.github.com>
2023-11-17 18:00:05 +0800

    Change the url path heirarchy

--- src/app/views/rp-related-companies/rp-related-companies-routing.module.ts
+++ src/app/views/rp-related-companies/rp-related-companies-routing.module.ts
@@ -5,19 +5,19 @@ import { RpRelatedCompaniesComponent } from './rp-related-companies.component';
 
 const routes: Routes = [
     {
-      path: '',
-      data: {
-        title: 'Affiliates of Related Party',
-      },
-      children: [
-        {
+      // path: '',
+      // data: {
+      //   title: 'Affiliates of Related Party',
+      // },
+      // children: [
+      //   {
           path: '',
           component: RpRelatedCompaniesComponent,
           data: {
             title: 'Other Related Companies ',
           },
-        },
-      ]
+      //   },
+      // ]
     },
   ];
 
