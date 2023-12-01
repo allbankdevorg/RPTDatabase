6b5a4d90660587ff8451d10dcb727d7a6a5befe8
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-01 17:57:49 +0800

    Added the logout function that will be bind in the logout button

--- src/app/containers/default-layout/default-header/default-header.component.ts
+++ src/app/containers/default-layout/default-header/default-header.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
+import { AuthSessionService } from 'src/app/services/authentication/auth-session.service';
+import {Logout} from '../../../functions-files/confirm-logout';
+import { SessionTimeoutService } from 'src/app/services/useridle/session-timeout.service';
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
 
 @Component({
@@ -15,7 +17,25 @@ export class DefaultHeaderComponent extends HeaderComponent {
   public newTasks = new Array(5)
   public newNotifications = new Array(5)
 
-  constructor(private classToggler: ClassToggleService) {
+  constructor(
+    private classToggler: ClassToggleService,
+    public authService: AuthSessionService,
+    private idle: SessionTimeoutService) {
     super();
   }
+
+  ngOnInt() {
+    // this.idle.timedOut()
+  }
+
+  // Simulate logout
+  Logout(): void {
+    const userConfirmed = window.confirm("Are You Sure You Want To Logout?");
+
+  if (userConfirmed) {
+    this.authService.simulateLogout();
+  }
+    
+  }
+
 }
