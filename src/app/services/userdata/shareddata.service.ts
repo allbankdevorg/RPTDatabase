6a6d0903df65314a1f8a97bbd26a4cfa705e9884
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-01 18:02:33 +0800

    Created a shareddata service to temporary stored the user data then share it among the components

---
+++ src/app/services/userdata/shareddata.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShareddataService {
+  
+  private userDataKey = 'userData';
+
+  private userDataSubject = new BehaviorSubject<any>(null);
+  userData$ = this.userDataSubject.asObservable();
+
+  getUserData(): any {
+    const storedUserData = sessionStorage.getItem(this.userDataKey);
+    return storedUserData ? JSON.parse(storedUserData) : null;
+  }
+
+  setUserData(user: any): void {
+    sessionStorage.setItem(this.userDataKey, JSON.stringify(user));
+    this.userDataSubject.next(user);
+  }
+
+  clearUserData(): void {
+    sessionStorage.removeItem(this.userDataKey);
+    this.userDataSubject.next(null);
+  }
+}
