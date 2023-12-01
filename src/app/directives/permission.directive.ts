0d59f465db434e9ba37577dc01c5af1213c244f5
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-01 17:58:33 +0800

    Make the User data to be the data of the user that has login

--- src/app/directives/permission.directive.ts
+++ src/app/directives/permission.directive.ts
@@ -2,23 +2,37 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
+import { AuthSessionService } from '../services/authentication/auth-session.service';
+import { ShareddataService } from '../services/userdata/shareddata.service'; // Adjust the import path
+
+
+
+interface Authority {
+  access?: string;
+  [key: string]: number | string | undefined;
+}
+
 @Directive({
   selector: '[appHasPermission]'
 })
 export class HasPermissionDirective {
   private permissions: string[] = [];
   private allowedRoute: string | undefined;
+  private user: any; // Change to dynamic user data
 
   @Input() set appHasPermission(value: string) {
     this.permissions = value.split(',');
     this.updateView();
   }
   
+  
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private authSessionService: AuthSessionService,
+    private userDataService: ShareddataService 
   ) {
     // Subscribe to route changes
     // this.router.events.pipe(
@@ -26,7 +40,22 @@ export class HasPermissionDirective {
     // ).subscribe(() => {
     //   this.updateView();
     // });
+    this.user = this.authSessionService.getUserData();
+  // this.userDataService.userData$.subscribe(userData => {
+  //   this.user = userData;
+  //   // this.updateView();
+  // });
   }
+
+  // ngOnInit(): void {
+  //   // Subscribe to changes in user data
+  //   this.userDataService.userData$.subscribe(userData => {
+  //     // Update the view whenever user data changes
+  //     this.updateView();
+  //   });
+  // }
+
+
 
   private updateView(): void {
     // Get the current route from the activated route
@@ -40,38 +69,50 @@ export class HasPermissionDirective {
   }
 
   private checkPermission(): boolean {
-    // Find the authority that matches the current route
-    const matchingAuthority = this.user.authority.find(authority => authority.access.toLowerCase() === this.allowedRoute);
+    // Ensure that this.user.authority is an array before using find
+    if (Array.isArray(this.user.authority)) {
+      // Find the authority that matches the current route
+      const matchingAuthority = this.user.authority.find((authority: Authority) => authority?.access?.toLowerCase() === this.allowedRoute);
 
-    console.log(this.allowedRoute);
-    // Check if matching authority is found
-    if (matchingAuthority) {
-        // Check if all permissions are granted
-        const allPermissionsGranted = this.permissions.every(permission => matchingAuthority[permission] === 1);
-        return allPermissionsGranted;
-    }
+      console.log(this.allowedRoute);
 
-    return false;
+      // Check if matching authority is found
+      if (matchingAuthority) {
+          // Check if all permissions are granted
+          const allPermissionsGranted = this.permissions.every(permission => matchingAuthority[permission] === 1);
+          return allPermissionsGranted;
+      }
+  }
+
+  // Handle the case when this.user.authority is not an array or is empty
+  return false;
 }
 
   // Replace 'user' with the actual variable representing your user data
-  private user = {
-    id: 1,
-    fName: 'Yiorgos Avraamu',
-    mName: 'New',
-    lName: 'Avraamu',
-    userName: 'User1',
-    email: 'test@email.com',
-    mobile: 1231244,
-    department: 'ITG',
-    role: 'maker',
-    authority: [
-      { access: 'dri', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
-      { access: 'users', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
-      { access: 'bankofficer', view: 1, add: 0, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
-      { access: 'directorsrelated/:id', view: 1, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
-    ],
-  };
+  // private user = {
+  //   id: 1,
+  //   fName: 'Yiorgos Avraamu',
+  //   mName: 'New',
+  //   lName: 'Avraamu',
+  //   userName: 'User1',
+  //   email: 'test@email.com',
+  //   mobile: 1231244,
+  //   department: 'ITG',
+  //   role: 'maker',
+  //   authority: [
+  //     { access: 'dri', view: 1, add: 1, edit: 1, delete: 1, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'directorsrelated/:id', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'bankofficer', view: 1, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'bankstockholders', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'affiliates', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'affiliates-related-companies', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'other-related-parties', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'rp-officer', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'pac/:id', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'rpofficer-ri/:id', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //     { access: 'users', view: 0, add: 1, edit: 1, delete: 0, maker: 1, approver: 0, reviewer: 1 },
+  //   ] ,
+  // };
 }
 
 
