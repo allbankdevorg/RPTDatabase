2af9d7f7abbaa30f15762ef55ed93db2a13c8fc2
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-01 18:03:31 +0800

    Remove the code to store data in the local storage then modify the idle time

--- src/app/services/useridle/session-timeout.service.ts
+++ src/app/services/useridle/session-timeout.service.ts
@@ -1,31 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Idle, DEFAULT_INTERRUPTSOURCES, } from '@ng-idle/core';
-
+// Services
+import { AuthSessionService } from '../authentication/auth-session.service';
 @Injectable({
   providedIn: 'root'
 })
 export class SessionTimeoutService {
 
   constructor(private idle: Idle, 
-    private router: Router) {
+    private router: Router,
+    private authService : AuthSessionService) {
     this.setIdleConfig();
   }
 
   private setIdleConfig(): void {
-    this.idle.setIdle(300); // 5 minutes
+    // this.idle.setIdle(5); // 5 minutes
     this.idle.setTimeout(3); // 3 sec
     this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
     // this.keepalive.interval(15); // 15 seconds
+    
+    this.idle.watch();
+    this.idle.onIdleEnd.subscribe(() => {
+      console.log('No longer idle.');
+      this.resetIdleTimer();
+    });
 
-    this.idle.onIdleEnd.subscribe(() => console.log('No longer idle.'));
     this.idle.onTimeout.subscribe(() => {
       console.log('Session expired!');
       // Implement your logout logic here
-      this.logout();
+      // this.timedOut();
     });
 
-    this.idle.watch();
   }
 
   resetIdleTimer(): void {
@@ -33,16 +39,14 @@ export class SessionTimeoutService {
     // this.keepalive.ping();
   }
 
-  private logout(): void {
+  public timedOut(): void {
     // Implement your logout logic here
     // For example, clear authentication-related data and redirect to the login page
     console.log('Logging out...');
     // Clear authentication data (e.g., tokens)
-    localStorage.removeItem('authToken');
-    localStorage.removeItem('userId');
-    localStorage.removeItem('userRole');
+    this.authService.simulateLogout();
     // Redirect to the login page
     // Note: You need to implement the Router or navigate as per your application's structure
-    // this.router.navigate(['/login']);
+    this.router.navigate(['/login']);
   }
 }
