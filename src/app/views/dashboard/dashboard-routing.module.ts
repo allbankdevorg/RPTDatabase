0b6a4f4d9c97d6dd93a9676a6b5511f5eb92fbad
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-01 18:11:50 +0800

    Added the route Guard to the routing to protect the routes from unauthorized access

--- src/app/views/dashboard/dashboard-routing.module.ts
+++ src/app/views/dashboard/dashboard-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { AuthGuard } from 'src/app/guard/auth-guard.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    // canActivate: [AuthGuard],
     data: {
       title: $localize`Dashboard`
     }
