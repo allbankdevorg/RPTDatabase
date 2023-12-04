a4c3626c51ccaf44776fd3290ce96f100f5adbb2
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-04 14:00:34 +0800

    Change the data format of the navigation items

--- src/app/containers/default-layout/_nav.ts
+++ src/app/containers/default-layout/_nav.ts
@@ -1,329 +1,154 @@
+
+
 import { INavData } from '@coreui/angular';
 
-export const navItems: INavData[] = [
+
+export const navItems: INavData [] = [
   {
-    name: 'Dashboard',
-    url: '/dashboard',
-    iconComponent: { name: 'cil-speedometer' },
-    // badge: {
-    //   color: 'info',
-    //   text: 'NEW'
-    // },
-  },
-  // {
-  //   title: true,
-  //   name: ''
-  // },
-  {
-      name: 'DOSRI',
-      url: '/',
-      children: [
-            {
-            name: 'Directors Related Interest',
-            url: '/dosri/dri',
-            iconComponent: { name: '' },
-            },
-            {
-              name: 'Bank Officer',
-              url: '/dosri/bankofficer',
-              iconComponent: { name: '' }
-            },
-            {
-              name: 'Banks Stockholder',
-              url: '/dosri/bankstockholders',
-              iconComponent: { name: '' }
-            },
-          ]
+      "name": "Dashboard",
+      "url": "/dashboard"
   },
   {
-    name: 'Affiliates and RP',
-    url: '/',
-    iconComponent: { name: '' },
-    children: [
+      "name": "DOSRI",
+      "url": "/",
+      "children": [
           {
-            name: 'Affiliates',
-            url: '/arp/affiliates',
-            iconComponent: { name: '' },
+              "name": "Directors Related Interest",
+              "url": "/dosri/dri"
           },
           {
-            name: 'Other Related Companies',
-            url: '/arp/affiliates-related-companies',
-            iconComponent: { name: '' },
+              "name": "Bank Officer",
+              "url": "/dosri/bankofficer"
           },
           {
-            name: 'Other RC Under JMN',
-            url: '/arp/other-related-parties',
-            iconComponent: { name: '' },
-          },
-          {
-            name: 'Other RP Officers',
-            url: '/arp/rp-officer',
-            iconComponent: { name: '' },
-          },
-        ]
+              "name": "Banks Stockholder",
+              "url": "/dosri/bankstockholders"
+          }
+      ]
   },
   {
-    name: 'Maintenance',
-    url: '/',
-    children: [
+      "name": "Affiliates and RP",
+      "url": "/",
+      "children": [
           {
-          name: 'Users',
-          url: '/maintenance/users',
-          iconComponent: { name: '' },
+              "name": "Affiliates",
+              "url": "/arp/affiliates"
           },
           {
-            name: 'Settings',
-            url: '/maintenance/settings',
-            iconComponent: { name: '' }
+              "name": "Other Related Companies",
+              "url": "/arp/affiliates-related-companies"
           },
           {
-            name: 'Manuals',
-            url: '/maintenance/manuals',
-            iconComponent: { name: '' }
+              "name": "Other RC Under JMN'",
+              "url": "/arp/other-related-parties"
           },
-        ]
-},
+          {
+              "name": "Other RP Officers",
+              "url": "/arp/rp-officer"
+          }
+      ]
+  },
+  {
+      "name": "Maintenance",
+      "url": "/",
+      "children": [
+          {
+              "name": "Users",
+              "url": "/maintenance/users"
+          },
+          {
+              "name": "Settings",
+              "url": "/maintenance/settings"
+          },
+          // {
+          //     "name": "Manuals",
+          //     "url": "/maintenance/manuals"
+          // }
+      ]
+    }
+]
+
+
+
+// export const navItems: INavData[] = [
+//   {
+//     name: 'Dashboard',
+//     url: '/dashboard',
+//     iconComponent: { name: 'cil-speedometer' },
+//   },
+//   {
+//       name: 'DOSRI',
+//       url: '/',
+//       iconComponent: { name: '' },
+//       children: [
+//             {
+//             name: 'Directors Related Interest',
+//             url: '/dosri/dri',
+//             iconComponent: { name: '' },
+//             },
+//             {
+//               name: 'Bank Officer',
+//               url: '/dosri/bankofficer',
+//               iconComponent: { name: '' }
+//             },
+//             {
+//               name: 'Banks Stockholder',
+//               url: '/dosri/bankstockholders',
+//               iconComponent: { name: '' }
+//             },
+//           ]
+//   },
+//   {
+//     name: 'Affiliates and RP',
+//     url: '/',
+//     iconComponent: { name: '' },
+//     children: [
+//           {
+//             name: 'Affiliates',
+//             url: '/arp/affiliates',
+//             iconComponent: { name: '' },
+//           },
+//           {
+//             name: 'Other Related Companies',
+//             url: '/arp/affiliates-related-companies',
+//             iconComponent: { name: '' },
+//           },
+//           {
+//             name: 'Other RC Under JMN',
+//             url: '/arp/other-related-parties',
+//             iconComponent: { name: '' },
+//           },
+//           {
+//             name: 'Other RP Officers',
+//             url: '/arp/rp-officer',
+//             iconComponent: { name: '' },
+//           },
+//         ]
+//   },
+//   {
+//     name: 'Maintenance',
+//     url: '/',
+//     iconComponent: { name: '' },
+//     children: [
+//           {
+//           name: 'Users',
+//           url: '/maintenance/users',
+//           iconComponent: { name: '' },
+//           },
+//           {
+//             name: 'Settings',
+//             url: '/maintenance/settings',
+//             iconComponent: { name: '' }
+//           },
+//           {
+//             name: 'Manuals',
+//             url: '/maintenance/manuals',
+//             iconComponent: { name: '' }
+//           },
+//         ]
+// },
  
   
   
-  // {
-  //   name: 'Typography',
-  //   url: '/theme/typography',
-  //   linkProps: { fragment: 'someAnchor' },
-  //   iconComponent: { name: 'cil-pencil' }
-  // },
-  // {
-  //   name: 'Components',
-  //   title: true
-  // },
-  // {
-  //   name: 'Base',
-  //   url: '/base',
-  //   iconComponent: { name: 'cil-puzzle' },
-  //   children: [
-  //     {
-  //       name: 'Accordion',
-  //       url: '/base/accordion'
-  //     },
-  //     {
-  //       name: 'Breadcrumbs',
-  //       url: '/base/breadcrumbs'
-  //     },
-  //     {
-  //       name: 'Cards',
-  //       url: '/base/cards'
-  //     },
-  //     {
-  //       name: 'Carousel',
-  //       url: '/base/carousel'
-  //     },
-  //     {
-  //       name: 'Collapse',
-  //       url: '/base/collapse'
-  //     },
-  //     {
-  //       name: 'List Group',
-  //       url: '/base/list-group'
-  //     },
-  //     {
-  //       name: 'Navs & Tabs',
-  //       url: '/base/navs'
-  //     },
-  //     {
-  //       name: 'Pagination',
-  //       url: '/base/pagination'
-  //     },
-  //     {
-  //       name: 'Placeholder',
-  //       url: '/base/placeholder'
-  //     },
-  //     {
-  //       name: 'Popovers',
-  //       url: '/base/popovers'
-  //     },
-  //     {
-  //       name: 'Progress',
-  //       url: '/base/progress'
-  //     },
-  //     {
-  //       name: 'Spinners',
-  //       url: '/base/spinners'
-  //     },
-  //     {
-  //       name: 'Tables',
-  //       url: '/base/tables'
-  //     },
-  //     {
-  //       name: 'Tabs',
-  //       url: '/base/tabs'
-  //     },
-  //     {
-  //       name: 'Tooltips',
-  //       url: '/base/tooltips'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Buttons',
-  //   url: '/buttons',
-  //   iconComponent: { name: 'cil-cursor' },
-  //   children: [
-  //     {
-  //       name: 'Buttons',
-  //       url: '/buttons/buttons'
-  //     },
-  //     {
-  //       name: 'Button groups',
-  //       url: '/buttons/button-groups'
-  //     },
-  //     {
-  //       name: 'Dropdowns',
-  //       url: '/buttons/dropdowns'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Forms',
-  //   url: '/forms',
-  //   iconComponent: { name: 'cil-notes' },
-  //   children: [
-  //     {
-  //       name: 'Form Control',
-  //       url: '/forms/form-control'
-  //     },
-  //     {
-  //       name: 'Select',
-  //       url: '/forms/select'
-  //     },
-  //     {
-  //       name: 'Checks & Radios',
-  //       url: '/forms/checks-radios'
-  //     },
-  //     {
-  //       name: 'Range',
-  //       url: '/forms/range'
-  //     },
-  //     {
-  //       name: 'Input Group',
-  //       url: '/forms/input-group'
-  //     },
-  //     {
-  //       name: 'Floating Labels',
-  //       url: '/forms/floating-labels'
-  //     },
-  //     {
-  //       name: 'Layout',
-  //       url: '/forms/layout'
-  //     },
-  //     {
-  //       name: 'Validation',
-  //       url: '/forms/validation'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Charts',
-  //   url: '/charts',
-  //   iconComponent: { name: 'cil-chart-pie' }
-  // },
-  // {
-  //   name: 'Icons',
-  //   iconComponent: { name: 'cil-star' },
-  //   url: '/icons',
-  //   children: [
-  //     {
-  //       name: 'CoreUI Free',
-  //       url: '/icons/coreui-icons',
-  //       badge: {
-  //         color: 'success',
-  //         text: 'FREE'
-  //       }
-  //     },
-  //     {
-  //       name: 'CoreUI Flags',
-  //       url: '/icons/flags'
-  //     },
-  //     {
-  //       name: 'CoreUI Brands',
-  //       url: '/icons/brands'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Notifications',
-  //   url: '/notifications',
-  //   iconComponent: { name: 'cil-bell' },
-  //   children: [
-  //     {
-  //       name: 'Alerts',
-  //       url: '/notifications/alerts'
-  //     },
-  //     {
-  //       name: 'Badges',
-  //       url: '/notifications/badges'
-  //     },
-  //     {
-  //       name: 'Modal',
-  //       url: '/notifications/modal'
-  //     },
-  //     {
-  //       name: 'Toast',
-  //       url: '/notifications/toasts'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Widgets',
-  //   url: '/widgets',
-  //   iconComponent: { name: 'cil-calculator' },
-  //   badge: {
-  //     color: 'info',
-  //     text: 'NEW'
-  //   }
-  // },
-  // {
-  //   title: true,
-  //   name: 'Extras'
-  // },
-  // {
-  //   name: 'Pages',
-  //   url: '/login',
-  //   iconComponent: { name: 'cil-star' },
-  //   children: [
-  //     {
-  //       name: 'Login',
-  //       url: '/login'
-  //     },
-  //     {
-  //       name: 'Register',
-  //       url: '/register'
-  //     },
-  //     {
-  //       name: 'Error 404',
-  //       url: '/404'
-  //     },
-  //     {
-  //       name: 'Error 500',
-  //       url: '/500'
-  //     }
-  //   ]
-  // },
-  // {
-  //   title: true,
-  //   name: 'Links',
-  //   class: 'py-0'
-  // },
-  // {
-  //   name: 'Docs',
-  //   url: 'https://coreui.io/angular/docs/templates/installation',
-  //   iconComponent: { name: 'cil-description' },
-  //   attributes: { target: '_blank', class: '-text-dark' },
-  //   class: 'mt-auto'
-  // },
-  // {
-  //   name: 'Try CoreUI PRO',
-  //   url: 'https://coreui.io/product/angular-dashboard-template/',
-  //   iconComponent: { name: 'cil-layers' },
-  //   attributes: { target: '_blank' }
-  // }
-];
+ 
+// ];
