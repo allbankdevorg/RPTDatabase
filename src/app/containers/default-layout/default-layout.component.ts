e6e9677baea195ef34b528b410b755816395c403
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-05 08:30:31 +0800

    Change the source of the Navigation Menu

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import {NavItemsService} from '../../services/nav-items/nav-items.service';
+// import { navItems } from './_nav';
 
-import { navItems } from './_nav';
+
+// Functions Import
+import {getNavi} from '../../functions-files/getFunctions';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +13,39 @@ import { navItems } from './_nav';
 })
 export class DefaultLayoutComponent {
 
-  public navItems = navItems;
+  public navItems = [];
 
-  constructor() {}
+  constructor(private nI: NavItemsService) {
+   
+
+  }
+
+  ngOnInit() {
+    
+    this.getNav();
+   
+    // console.log(this.orgData);
+  }
+
+
+
+
+  getNav() {
+    getNavi((navItems) => {
+      // Process the data to count directors related to each company
+        
+
+        // Set the data source for your MatTable
+        const navLink = navItems;
+        this.navItems = navItems
+        // console.log(navLink);
+   
+
+      
+      });
+  }
+
+  
+
+  
 }
