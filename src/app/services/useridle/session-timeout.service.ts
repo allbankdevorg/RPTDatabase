0fed18957298b49bb47dc4bb9a1a89e4c4afd6c9
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-06 18:04:56 +0800

    Fix the session-timeout function

--- src/app/services/useridle/session-timeout.service.ts
+++ src/app/services/useridle/session-timeout.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Idle, DEFAULT_INTERRUPTSOURCES, } from '@ng-idle/core';
 
 import { Keepalive } from '@ng-idle/keepalive'; 
 // Services
 import { AuthSessionService } from '../authentication/auth-session.service';
+
+import { DOCUMENT } from '@angular/common';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,16 +16,37 @@ export class SessionTimeoutService {
   constructor(private idle: Idle, 
     private router: Router,
     private authService : AuthSessionService,
-    private keepalive: Keepalive) {
-    this.setIdleConfig();
-  }
+    private keepalive: Keepalive,
+    @Inject(DOCUMENT) private document: Document) {
+    // this.setIdleConfig();
+    // Reset idle timer on mouse move
+    if (this.authService.isAuthenticated()) {
+      this.setIdleConfig();
+    }
+  
+}
 
-  private setIdleConfig(): void {
-    this.idle.setIdle(300); // 5 minutes
-    this.idle.setTimeout(3); // 3 sec
+  public setIdleConfig(): void {
+    this.idle.setIdle(5); // 5 minutes
+    this.idle.setTimeout(60); // 3 sec
     this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
     this.keepalive.interval(5); // 15 seconds
     
+    // this.document.body.addEventListener('mousemove', () => {
+    //   this.resetIdleTimer(); 
+    //   console.log("Hover, Idle reset")
+    // });
+  
+    this.idle.onIdleStart.subscribe(() => {
+      console.log('Entered idle mode');  
+    });
+  
+    this.idle.onIdleEnd.subscribe(() => {
+      console.log('Exited idle mode');
+      this.resetIdleTimer();
+    });
+    
+
     this.idle.watch();
     this.idle.onIdleEnd.subscribe(() => {
       console.log('No longer idle.');
