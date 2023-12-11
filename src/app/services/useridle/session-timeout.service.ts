9d803dbc3cddcee079ca929569192dda9338eb77
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-11 18:06:54 +0800

    Change the idle time to 60 seconds

--- src/app/services/useridle/session-timeout.service.ts
+++ src/app/services/useridle/session-timeout.service.ts
@@ -28,7 +28,7 @@ export class SessionTimeoutService {
 
   public setIdleConfig(): void {
     this.idle.setIdle(10); // 5 minutes
-    this.idle.setTimeout(1200); // 3 sec
+    this.idle.setTimeout(60); // 3 sec
     this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
     this.keepalive.interval(5); // 15 seconds
     
