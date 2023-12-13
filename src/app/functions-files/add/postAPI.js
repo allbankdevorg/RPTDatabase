26a4b04490c9f7e6d79262d91c0f6a41fe52319e
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-13 13:40:35 +0800

    Wrap the content in a Promise

--- src/app/functions-files/add/postAPI.js
+++ src/app/functions-files/add/postAPI.js
@@ -18,28 +18,50 @@
  * @param {any} formData - The form data containing cisNumber, accountName, and companyName.
  */
 function createDosri(formData) {
+  return new Promise((resolve, reject) => {
     console.log(formData)
-   var settings = {
-     "url": "http://10.232.236.15:8092/api/addData",
-     "method": "POST",
-     "timeout": 0,
-     "headers": {
-       "Content-Type": "application/json"
-     },
-     "data": JSON.stringify({
-       "cmd": 1,
-       "request": {
-             "cis_number": formData.cisNumber,       // Use form data
-             "account_name": formData.accountName,   // Use form data
-             "company_name": formData.companyName    // Use form data
-           }
-     }),
-   };
+    var settings = {
+      "url": "http://10.232.236.15:8092/api/addData",
+      "method": "POST",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": JSON.stringify({
+        "cmd": 1,
+        "request": {
+              "cis_number": formData.cisNumber,       // Use form data
+              "account_name": formData.accountName,   // Use form data
+              "company_name": formData.companyName    // Use form data
+            }
+      }),
+    };
+ 
 
+    $.ajax(settings).done(function (response) {
+      // Log the response
+      console.log(response.result[0].status);
+      
+      // Check the status and resolve/reject the promise accordingly
+      Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
+      if (response.result[0].status === 'success') {
+        resolve(response);
+      } else {
+        reject(response);
+      }
+    });
+  });
+
+  return new Promise((resolve, reject) => {
+    console.log(formData)
+    console.log(moduleV);
+    console.log(formData)
+   
    
    $.ajax(settings).done(function (response) {
      Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
    });
+  });
 }
 
 
