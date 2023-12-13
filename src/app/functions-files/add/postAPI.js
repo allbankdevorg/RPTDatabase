e19fc845e6fda2ccd97243c4c175c0b3c2ad16d8
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-13 18:36:13 +0800

    Wrap the code into a Promise

--- src/app/functions-files/add/postAPI.js
+++ src/app/functions-files/add/postAPI.js
@@ -52,16 +52,6 @@ function createDosri(formData) {
     });
   });
 
-  return new Promise((resolve, reject) => {
-    console.log(formData)
-    console.log(moduleV);
-    console.log(formData)
-   
-   
-   $.ajax(settings).done(function (response) {
-     Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
-   });
-  });
 }
 
 
@@ -72,31 +62,41 @@ function createDosri(formData) {
  * @param {any} selectedCompCISNumber - The selected company's CIS number.
  */
  function createDirectors(directData, selectedCompCISNumber) {
+  return new Promise((resolve, reject) => {
     console.log(selectedCompCISNumber);
-    
-      var settings = {
-        "url": "http://10.232.236.15:8092/api/addData",
-        "method": "POST",
-        "timeout": 0,
-        "headers": {
-          "Content-Type": "application/json"
-        },
-        "data": JSON.stringify({
-          "cmd": 2,
-          "request": {
-            "cis_number": directData.cisNumber,
-            "fname": directData.dFirstName,
-            "mname": directData.dMiddleName,
-            "lname": directData.dLastName,
-            "position": directData.dPosition,
-            "com_cisnumber": selectedCompCISNumber
-          }
-        }),
-      };
+    var settings = {
+      "url": "http://10.232.236.15:8092/api/addData",
+      "method": "POST",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": JSON.stringify({
+        "cmd": 2,
+        "request": {
+          "cis_number": directData.cisNumber,
+          "fname": directData.dFirstName,
+          "mname": directData.dMiddleName,
+          "lname": directData.dLastName,
+          "position": directData.dPosition,
+          "com_cisnumber": selectedCompCISNumber
+        }
+      }),
+    };
+
+    $.ajax(settings).done(function (response) {
+      // Log the response
+      console.log(response.result[0].status);
       
-      $.ajax(settings).done(function (response) {
-        Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
-      });// Implement code to insert a new director into the database
+      // Check the status and resolve/reject the promise accordingly
+      Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
+      if (response.result[0].status === 'success') {
+        resolve(response);
+      } else {
+        reject(response);
+      }
+    });
+  });
 }
 
 
@@ -107,7 +107,7 @@ function createDosri(formData) {
  * @param {any} selectedDirCisNumber - The selected director's CIS number.
 */
 function createRelatedInterest(riData, buttonId, selectedDirCisNumber) {
-
+  return new Promise((resolve, reject) => {
     console.log(buttonId)
     console.log(selectedDirCisNumber)
     console.log(riData)
@@ -130,11 +130,22 @@ function createRelatedInterest(riData, buttonId, selectedDirCisNumber) {
         }
       }),
     };
-    
+ 
+
     $.ajax(settings).done(function (response) {
+      // Log the response
+      console.log(response.result[0].status);
+      console.log(response.result[0].message);
+      
+      // Check the status and resolve/reject the promise accordingly
       Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
+      if (response.result[0].status === 'success') {
+        resolve(response);
+      } else {
+        reject(response);
+      }
     });
-    // Implement code to insert a new director into the database
+  });
 }
 
 
