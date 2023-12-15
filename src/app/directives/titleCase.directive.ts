21539fd13ca7f33e57539958f69c1d72d93fc652
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-15 17:35:11 +0800

    Fix the directives to retain the format even when the data are submitted

--- src/app/directives/titleCase.directive.ts
+++ src/app/directives/titleCase.directive.ts
@@ -1,16 +1,43 @@
 // title-case.directive.ts
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
-  selector: '[appTitleCase]'
+  selector: '[appTitleCase]',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TitleCaseDirective),
+      multi: true
+    }
+  ]
 })
-export class TitleCaseDirective {
+export class TitleCaseDirective implements ControlValueAccessor {
 
-  constructor(private el: ElementRef) {}
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('input', ['$event.target.value'])
   onInput(value: string) {
-    this.el.nativeElement.value = this.toTitleCase(value);
+    const transformedValue = this.toTitleCase(value);
+    this.renderer.setProperty(this.el.nativeElement, 'value', transformedValue);
+    this.onChange(transformedValue);
+  }
+
+  writeValue(value: any): void {
+    const transformedValue = this.toTitleCase(value);
+    this.renderer.setProperty(this.el.nativeElement, 'value', transformedValue);
+    this.onChange(transformedValue);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 
   private toTitleCase(input: string): string {
