c2bcab8529a37b9f1c21408acd48a3ad18916bef
01Junel <52649280+01Junel@users.noreply.github.com>
2023-12-18 18:01:53 +0800

    Imported the Imports for DirRIService and  other imports for the New Modal

--- src/app/views/dosri/directorsrelated/directorsrelated.component.ts
+++ src/app/views/dosri/directorsrelated/directorsrelated.component.ts
@@ -17,6 +17,7 @@ import { Injectable } from '@angular/core';
 // Services
 import { DataTransferService } from '../../../services/data-transfer.service';
 import { SharedService } from '../dataintegration/shared.service';
+import {DirRIService} from '../../../services/dirRI/dir-ri.service'; //Service to set the value of the DirCIS and buttonID in adding RI of Directors
 
 // Imports for Functions
 import {createDirectors} from '../../../functions-files/add/postAPI';
@@ -29,6 +30,13 @@ import { FetchDataService } from 'src/app/services/fetch/fetch-data.service';
 // Audit Trail
 import { AuditTrailService } from '../../../services/auditTrail/audit-trail.service';
 import {AuditTrail} from '../../../model/audit-trail.model';
+
+// For Modal
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import { DirectorsRIModalComponent } from 'src/app/modal-dialog/directors-ri-modal/directors-ri-modal.component';
+import { DirectorsModalComponent } from 'src/app/modal-dialog/directors-modal/directors-modal.component';
+import { forkJoin } from 'rxjs';
+
 
 export interface Child {
   name: string;
@@ -72,11 +80,8 @@ export class DirectorsrelatedComponent {
   positionOptions: TooltipPosition = 'right';
   companyDetails: any; //a variable to hold the fetched company details:\
   sharedData: string | any;
-  // relationShipModal: any;
-  drctrForm: FormGroup;
-  riForm: FormGroup;
-  buttonId: number = 0;
-  selectedDirCisNumber: number = 0;
+  // buttonId: number = 0;
+  // selectedDirCisNumber: number = 0;
   selectedCompCISNumber: number = 0;
   directorsData: Director[] = []; // This should be populated with your director data
   filteredDirectors: Director[] = [];
@@ -94,36 +99,23 @@ export class DirectorsrelatedComponent {
    directorData: Director[] = [];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild('relationShipModal', { static: true }) relationShipModal: any;
+  // @ViewChild('relationShipModal', { static: true }) relationShipModal: any;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 
   constructor(private router: Router,
-              private formBuilder: FormBuilder, 
+              public _dialog: MatDialog,  
               private http: HttpClient,
               private sharedService: SharedService,
-              private dataTransferService: DataTransferService,
+              private dataService: DirRIService,
               private route: ActivatedRoute,
               private changeDetectorRef: ChangeDetectorRef,
               private ngZone: NgZone,
               private get: FetchDataService,
               private auditTrailService: AuditTrailService)
   {
-    this.drctrForm = this.formBuilder.group({
-      cisNumber: ['', [Validators.required]],
-      dFirstName: ['', [Validators.required]],
-      dMiddleName: [''],
-      dLastName: ['', [Validators.required]],
-      dPosition: ['', [Validators.required]],
-    });
-    this.riForm = this.formBuilder.group({
-      riCisNumber: [''],
-      riFirstName: ['', [Validators.required]],
-      riMiddleName: [''],
-      riLastName: ['', [Validators.required]],
-    });
     
     this.updateTableData();
     this.route.params.subscribe(params => {
@@ -132,12 +124,18 @@ export class DirectorsrelatedComponent {
 
       this.get.getCompany().subscribe((compData) => {
         // Process the data to count directors related to each company
-            const filteredCompany = compData.filter((company) => company.com_cis_number === this.compId);
+        if (compData) {
+          const filteredCompany = compData.filter((company) => company.com_cis_number === this.compId);
         
             for (const company of filteredCompany) {
               this.Company = company.com_company_name;
             }
-                  // Set the data source for your MatTable
+        }else {
+          // 
+          compData = [];
+        }
+            
+                  
       });
     });
   }
@@ -154,106 +152,72 @@ export class DirectorsrelatedComponent {
 
   // All Functions Below
   updateTableData(): void {
-     // Now that you have processed the data, you can fetch directors or perform any other operation
-     this.get.getDirectors().subscribe((directors) => {
-      const filteredDirectors = directors.filter((director) => director.com_related === this.compId);
-
-      const relationColumn = ['MothersName', 'FathersName', 'Spouse', 'Children', 'MotherinLaw', 'FatherinLaw'];
-
-      const tableData = filteredDirectors.map((director) => {
-        const row: Record<string, any> = {
-          'FullName': `${director.fname} ${director.mname}  ${director.lname}`,
-          'Company': this.Company,
-          'Position': director.position,
-          'dir_CisNumber': director.dir_cisnumber,
-          'comp_CIS': director.com_related,
-        };
-    
-        for (let index = 0; index < relationColumn.length; index++) {
-          const relationName = relationColumn[index];
-          const relatedData = director.related_interest
-            .filter(related => related.relation === index + 1)
-            .map(related => ({
-              fullName: `${related.fname} ${related.mname} ${related.lname}`,
-              cisNumber: related.cis_number,
-              dirRelated: related.dir_related
-            }))
-            .filter(data => typeof data.fullName === 'string' && data.fullName.trim() !== '');
-    
-          row[relationName] = relatedData;
-        }
-    
-        return row;
-      });
-    
-      this.dataSource = new MatTableDataSource(tableData);
-      // this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    
-      // Trigger change detection
-      this.changeDetectorRef.detectChanges();
+    // Now that you have processed the data, you can fetch directors or perform any other operation
+    this.get.getDirectors().subscribe((directors) => {
+      if (directors) {
+        
+            const filteredDirectors = directors.filter((director) => director.com_related === this.compId);
+        
+            const relationColumn = ['MothersName', 'FathersName', 'Spouse', 'Children', 'MotherinLaw', 'FatherinLaw'];
+        
+            const tableData = filteredDirectors.map((director) => {
+              const row: Record<string, any> = {
+                'FullName': `${director.fname} ${director.mname}  ${director.lname}`,
+                'Company': this.Company,
+                'Position': director.position,
+                'dir_CisNumber': director.dir_cisnumber,
+                'comp_CIS': director.com_related,
+              };
+        
+              for (let index = 0; index < relationColumn.length; index++) {
+                const relationName = relationColumn[index];
+        
+                // Check if director.related_interest is not null or undefined
+                const relatedData = director.related_interest
+                  ? director.related_interest
+                      .filter((related) => related && related.relation === index + 1)
+                      .map((related) => ({
+                        fullName: `${related.fname || ''} ${related.mname || ''} ${related.lname || ''}`,
+                        cisNumber: related.cis_number || '',
+                        dirRelated: related.dir_related || '',
+                      }))
+                      .filter((data) => typeof data.fullName === 'string' && data.fullName.trim() !== '')
+                  : [];
+        
+                row[relationName] = relatedData;
+              }
+        
+              return row;
+            });
+        
+            this.dataSource = new MatTableDataSource(tableData);
+            // this.dataSource.sort = this.sort;
+            this.dataSource.paginator = this.paginator;
+        
+            // Trigger change detection
+            this.changeDetectorRef.detectChanges();
+      }
+      else {
+        directors = [];
+      }
     });
-    
   }
-  // updateTableData(): void {
-  //   getDirectors((Director) => {
-  //     // const directorIdToDisplay = directorId;
-  //     console.log(Director);
-  //     // console.log('directorIdToDisplay:', directorIdToDisplay)
-  //     // console.log(companytoDisplay);
-  //     const filteredDirectors = Director.filter((director) => director.com_related === this.compId);
-      
-  //     const relationColumn = ['MothersName', 'FathersName', 'Spouse', 'Children', 'MotherinLaw', 'FatherinLaw'];
-  //     const tableData: Record<string, any>[] = [];
-      
-  //     for (const director of filteredDirectors) {
-  //         // const dir_relatedId = director.dir_cisnumber;
-  //         const row: Record<string, any> = {
-  //             'FullName': `${director.fname} ${director.mname}  ${director.lname}`,
-  //             'Company': this.Company,
-  //             'Position': director.position,
-  //             'dir_CisNumber': director.dir_cisnumber,
-  //             'comp_CIS': director.com_related,
-  //         };
-  //         // Loop through each element in the 'relationColumn' array
-  //         for (let index = 0; index < relationColumn.length; index++) {
-  //             const relationName = relationColumn[index]; // Get the current relation name from the 'relationColumn' array
-  //             // Filter 'director.related_interest' array to get related names based on the relation index
-  //             const relatedData = director.related_interest
-  //             .filter(related => related.relation === index + 1)
-  //             // Create an object with the required properties
-  //             .map(related => ({
-  //                 fullName: `${related.fname} ${related.mname} ${related.lname}`,
-  //                 cisNumber: related.cis_number,
-  //                 dirRelated: related.dir_related
-  //             }))
-  //             // Filter out objects with empty names (names with only whitespace)
-  //             .filter(data => typeof data.fullName === 'string' && data.fullName.trim() !== '');
-
-  //                   // Assign the 'relatedNames' array to the 'row' object with the key as 'relationName'
-  //                   row[relationName] = relatedData;
-
-  //         }
-      
-  //         tableData.push(row);
-
-  //     }
-      
-  //    this.dataSource.data = tableData;
-  //     console.log(this.dataSource.data);
-  //     // Trigger change detection
-  //     this.changeDetectorRef.detectChanges();
-  //   });
-    
-  // }
+  
+  
 
   
   setButtonId(id: number, dirCisNumber: number) {
-    this.buttonId = id;
-    this.selectedDirCisNumber = dirCisNumber;
+    // Set values in the service
+    console.log(id);
     console.log(dirCisNumber);
     
+    this.dataService.setDirCIS(dirCisNumber);
+    this.dataService.setButtonId(id);
+    
   }
+
+
+
 
   setComp() {
     this.selectedCompCISNumber = this.compId;
@@ -261,84 +225,6 @@ export class DirectorsrelatedComponent {
   }
 
 
-  setdirRelated() {
-    // director = director.dir_related;
-    // console.log(director);
-  }
-
-  // Adding Directors
-  onDSubmit() {
- 
-    if (this.drctrForm.valid) {
-      const directData = this.drctrForm.value;
-      const directorId = this.sharedService.getDirectorId();
-      const companyName = this.sharedService.getCompName();
-    
-      // Call the JavaScript function with form data
-      createDirectors(directData, this.selectedCompCISNumber)
-      .then((response) => {
-        this.logAction('Add', 'Successfuly Added Director', true, 'directorsrelated');
-        this.updateTableData();
-      })
-      .catch((error) => {
-        this.logAction('Add', 'Failed Adding Director', false, 'directorsrelated');
-        this.updateTableData();
-      }); // Pass the entire formData object
-      
-
-
-
-      
-    }
-
-    this.ngZone.run(() => {
-      this.dataSource.data = this.tableData;
-    });
-
-      // Trigger change detection
-    this.changeDetectorRef.detectChanges();
-    console.log(this.changeDetectorRef.detectChanges);
-    console.log(this.dataSource);
-  }
-
-  //Adding Related Interest 
-  onRISubmit() {
-    const directorId = this.sharedService.getDirectorId();
-    const companyName = this.sharedService.getCompName();
-    
-    if (this.riForm.valid) {
-      const riData = this.riForm.value;
-      
-      // Call the JavaScript function with form data
-      createRelatedInterest(riData, this.buttonId, this.selectedDirCisNumber) // Pass the entire formData object
-      .then((response) => {
-        this.logAction('Add', 'Successfuly Added Related Interest', true, 'directorsrelated');
-        this.updateTableData();
-      })
-      .catch((error) => {
-        this.logAction('Add', 'Failed Adding Related Interest', false, 'directorsrelated');
-        this.updateTableData();
-        console.log(this.dataSource);
-      });
-
-      
-    }
-
-    this.ngZone.run(() => {
-      this.dataSource.data = this.tableData;
-    });
-    
-      // Trigger change detection
-    this.changeDetectorRef.detectChanges();
-    console.log(this.changeDetectorRef.detectChanges);
-    console.log(this.dataSource);
-  }
-
-  //Showing Modal
-  onButtonClick() {
-    console.log('Show Modal Form');
-    
-  }
 
   // Getting the director_CISnumber
   display(row: any) {
@@ -353,21 +239,14 @@ export class DirectorsrelatedComponent {
   }
  
   delDirector(element: any, cisNumber: any, dirCisNumber: any) {
-    // console.log(element);
-    // console.log('CIS Number:', cisNumber);
-    // console.log('dir_related:', dirCisNumber);
-    // deleteDirector
+    
     deleteDOSRIDirector((dosriId) => {
 
     })
   }
 
   delRelationship(element: any, cis_number: string, dir_related: any): void {
-    // deleteRelationship()
-    // console.log(element);
-    // console.log('CIS Number:', cis_number);
-    // console.log('dir_related:', dir_related);
-    // console.log("Are you sure you want to delete?")
+    
     deleteDOSRIDirRelationship((dosriId) => {
 
     })
@@ -375,7 +254,63 @@ export class DirectorsrelatedComponent {
 
 
 
-  
+// Show Modal Form
+openAddEditEmpForm() {
+  const dialogRef = this._dialog.open(DirectorsRIModalComponent);
+  dialogRef.afterClosed().subscribe({
+    next: (val) => {
+      if (val) {
+        this.updateTableData();
+      }
+    },
+  });
+}
+
+openEditForm(data: any, event: any) {
+  event.stopPropagation();
+  console.log(data);
+  const dialogRef = this._dialog.open(DirectorsRIModalComponent, {
+    data,    
+  });
+
+  dialogRef.afterClosed().subscribe({
+    next: (val) => {
+      if (val) {
+        // this.getEmployeeList();
+        console.log("Successs");
+      }
+    },
+  });
+}
+
+
+openDirectorsForm() {
+  const dialogRef = this._dialog.open(DirectorsModalComponent);
+  dialogRef.afterClosed().subscribe({
+    next: (val) => {
+      if (val) {
+        this.updateTableData();
+      }
+    },
+  });
+}
+
+openEditDirForm(data: any, event: any) {
+  event.stopPropagation();
+  console.log(data);
+  const dialogRef = this._dialog.open(DirectorsModalComponent, {
+    data,    
+  });
+
+  dialogRef.afterClosed().subscribe({
+    next: (val) => {
+      if (val) {
+        // this.getEmployeeList();
+        console.log("Successs");
+      }
+    },
+  });
+} 
 
 
 
