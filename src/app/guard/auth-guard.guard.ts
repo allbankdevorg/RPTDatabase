d4cf044e12b427e442959d370d734bc838fef9cb
01Junel <52649280+01Junel@users.noreply.github.com>
2024-01-16 14:21:09 +0800

    Add the condition to remove access of user that is not Admin to the Route Maintenance

--- src/app/guard/auth-guard.guard.ts
+++ src/app/guard/auth-guard.guard.ts
@@ -5,6 +5,7 @@ import {
   Router,
   UrlTree,
 } from '@angular/router';
+// import { ToastrService } from 'ngx-toastr';
 import { AuthSessionService } from '../services/authentication/auth-session.service';
 import { Observable } from 'rxjs';
 @Injectable({
@@ -12,15 +13,30 @@ import { Observable } from 'rxjs';
 })
 export class AuthGuard {
   
-  constructor(public authService: AuthSessionService, public router: Router) {}
+  constructor(public authService: AuthSessionService, private router: Router) {}
   canActivate(
+    route: ActivatedRouteSnapshot,
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | UrlTree | boolean {
-    if (this.authService.isLoggedIn !== true) {
-      // window.alert('Access Denied, Login is Required to Access This Page!');
+    if (!this.authService.isLoggedIn) {
+      // Redirect to login page if not logged in
       this.router.navigate(['/login']);
+      return false;
     }
+
+    if (route.url.length > 0) {
+      let menu = route.url[0].path;
+      console.log(menu);
+      if (menu === 'maintenance' && this.authService.getRole() !== '4') {
+        // User with role other than '4' is trying to access 'maintenance/users'
+        // this.toastr.warning('You do not have access to this page.');
+        this.router.navigate(['/dashboard']); // Redirect to dashboard
+        return false;
+      }
+    }
+
     return true;
+  
   }
 }
