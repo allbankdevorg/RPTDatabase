4b0d53fee4a08da29e14650ee38624dd0b1860ac
01Junel <52649280+01Junel@users.noreply.github.com>
2024-01-17 17:21:39 +0800

    Update the Updating of the Company to include the hold_out and company Name

--- src/app/functions-files/update/updateAPI.js
+++ src/app/functions-files/update/updateAPI.js
@@ -11,7 +11,43 @@ function updateManagingCompany(formData) {
                 "cmd": 10,
                 "request": {
                     "cis_number": formData.aff_com_cis_number,
-                    "managing_company": formData.managing_company
+                    "managing_company": formData.managing_company,
+                    "account_name": formData.aff_com_comp_name,
+                    "hold_out": formData.hold_out   
+                }
+                }),
+            };
+
+            $.ajax(settings).done(function (response) {
+                
+                // Check the status and resolve/reject the promise accordingly
+                Swal.fire(`${response.result[0].message}`, ``, `${response.result[0].status}`);
+                if (response.result[0].status === 'success') {
+                resolve(response);
+                } else {
+                reject(response);
+                }
+            });
+    });
+}
+
+
+function updateCompany(formData) {
+    return new Promise((resolve, reject) => {
+            var settings = {
+                "url": "http://10.232.236.15:8092/api/updateData",
+                "method": "POST",
+                "timeout": 0,
+                "headers": {
+                "Content-Type": "application/json"
+                },
+                "data": JSON.stringify({
+                "cmd": 10,
+                "request": {
+                    "cis_number": formData.aff_com_cis_number,
+                    "managing_company": formData.managing_company,
+                    "account_name": formData.aff_com_company_name,
+                    "hold_out": formData.hold_out   
                 }
                 }),
             };
@@ -33,4 +69,5 @@ function updateManagingCompany(formData) {
 
 module.exports = {
     updateManagingCompany,
+    updateCompany
 }
