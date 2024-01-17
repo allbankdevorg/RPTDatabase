3a7cb6f2d3b934c76f1d20c32184edcdae2e67c4
01Junel <52649280+01Junel@users.noreply.github.com>
2024-01-17 17:31:26 +0800

    Update the updateTableData to add the holdout allocation for the PN data. And change the formula for the Outstanding Balance and Net Balance

--- src/app/views/simulation/rpt-list/rpt-list.component.ts
+++ src/app/views/simulation/rpt-list/rpt-list.component.ts
@@ -9,7 +9,7 @@ import { RPTSimulationModalComponent } from 'src/app/modal-dialog/rpt-simulation
 
 //Import for API Function
 import { FetchDataService } from 'src/app/services/fetch/fetch-data.service';
-
+import { HoldOutValue } from '../../../functions-files/add/postAPI';
 export interface RPTlist_model {
   loantype: string;
   cis_no: number;
@@ -36,10 +36,12 @@ export class RptListComponent {
   unimpairedCap: number = 1214764186.16;   //Unimpaired Capital
   definedRptRatio: number = 50;     //Pre defined Percentage
   availRptRatio: any;
-  approvedCapital: any;        // => the Loan approved Limit
+  approvedCapital: any;  // => the Loan approved Limit
+  totalHoldOut: number = 0;
+      
   
   displayedColumns: string[] = ['cis_no', 'loan_no', 'name', 'principal', 'principal_bal', 'loan_security', 
-  'DepositHO', 'netBal', 'tran_date', 'terms', 'purpose', 'intRate'];
+  'hold_out', 'netBal', 'date_granted', 'terms', 'purpose', 'intRate'];
   dataSource = new MatTableDataSource<any>([]);
   ToDisplay: string[] = [];
 
@@ -95,36 +97,69 @@ export class RptListComponent {
     this.updateTableData();
   }
 
-  updateTableData(): void {
-    this.get.getPNData((PNData) => {
-      if (PNData) {
-        // Use reduce to calculate the sum of "principal" values
-        const sumPrincipal = PNData.reduce((acc, obj) => {
-          acc.principal += parseFloat(obj.principal) || 0;
-          acc.principal_bal += parseFloat(obj.principal_bal) || 0;
-          return acc;
-        }, { principal: 0, principal_bal: 0 });
+ updateTableData(): void {
+      this.get.getPNData((PNData) => {
+        if (PNData) {
+          const uniqueCisNumbers = [...new Set(PNData.map((entry) => entry.cis_no))];
+          
 
-        this.rptBal = sumPrincipal.principal_bal;
-        const percentage = `${((this.rptBal / 1214764186.16) * 100).toFixed(2)}%`;
-        this.rptRatio = percentage;
-        // this.subtlOL = sumPrincipal.principal;
-        // this.subtlOB = this.rptBal;
-        this.ttlRPTOL = sumPrincipal.principal;
-        this.ttlRPTOB = this.rptBal;
-        this.dataSource.data = PNData;
-        this.availRptRatio = `${(this.definedRptRatio - parseFloat(percentage.replace('%', ''))).toFixed(2)}%`;
-        this.approvedCapital = this.unimpairedCap * 0.5;
-        
-        this.availBal = this.approvedCapital - this.rptBal;
-      } else {
+          uniqueCisNumbers.forEach((cisNumber) => {
+            HoldOutValue(cisNumber) // Pass the entire formData object
+            .then((response) => {
+              // Log the response when the promise is resolved
+                const holdOUT = response.result[0].Data[0].hold_out;
 
-      }
-    });
-  }
+                if (holdOUT) {
+                    // Distribute hold_out data equally among PN data entries
+                    const entries = PNData.filter((entry) => entry.cis_no === cisNumber);
+                    const holdOutValue = holdOUT || 0;
+                    const holdOutPerCis = entries.length > 0 ? holdOutValue / entries.length : 0;
+
+                    // Update PN Data with divided hold_out values
+                    entries.forEach((entry) => {
+                      entry.hold_out = Number(holdOutPerCis.toFixed(2));
+                      // console.log(entry.hold_out);
+                      this.totalHoldOut += entry.hold_out;
+                    });
+      
+                    // Update the dataSource with the combined data
+                    this.dataSource.data = PNData;
+                    // Calculate sums and ratios based on the filtered data
+                    const sumPrincipal = PNData.reduce((acc, obj) => {
+                              acc.principal += parseFloat(obj.principal) || 0;
+                              acc.principal_bal += parseFloat(obj.principal_bal) || 0;
+                              return acc;
+                            }, { principal: 0, principal_bal: 0 });
+                    
+                            this.rptBal = sumPrincipal.principal_bal - this.totalHoldOut;
+                            const percentage = `${((this.rptBal / 1214764186.16) * 100).toFixed(2)}%`;
+                            this.rptRatio = percentage;
+                            // this.subtlOL = sumPrincipal.principal;
+                            // this.subtlOB = this.rptBal;
+                            this.ttlRPTOL = sumPrincipal.principal;
+                            this.ttlRPTOB = sumPrincipal.principal_bal
+                            this.dataSource.data = PNData;
+                            this.availRptRatio = `${(this.definedRptRatio - parseFloat(percentage.replace('%', ''))).toFixed(2)}%`;
+                            this.approvedCapital = this.unimpairedCap * 0.5;
+                            
+                            this.availBal = this.approvedCapital - this.rptBal;
+
+                  } else {
+                    // Handle error or empty hold_out response
+                  }
+
+            })
+            .catch((error) => {
+             
+            });
+              
+          });
+        }
+      })
+ }
 
 
-    // Function to Show the simulation Modal
+   
     openSimulation() {
       const dialogRef = this._dialog.open(RPTSimulationModalComponent, {
         width: '50%', // Set the width as per your requirement
