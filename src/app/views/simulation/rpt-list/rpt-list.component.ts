47bb873b6586e941737d843d61aa97a340a36809
01Junel <52649280+01Junel@users.noreply.github.com>
2024-02-06 11:40:15 +0800

    remove commented code, and then modify the calculation of the simulation to accomodate the multi loan simulation process

--- src/app/views/simulation/rpt-list/rpt-list.component.ts
+++ src/app/views/simulation/rpt-list/rpt-list.component.ts
@@ -65,6 +65,7 @@ export class RptListComponent {
   SimulatedttlRPTOB: any;
   SimulatedapprovedCapital: any;
 
+  simulationPerformed: boolean = false;
   temporaryLoans?: Loan[];
   
   displayedColumns: string[] = ['loan_no', 'cis_no', 'name', 'principal', 'principal_bal', 'loan_security', 
@@ -128,228 +129,127 @@ export class RptListComponent {
     this.updateTableData();
     this.getUnimpairedCap();
     this.temporaryLoans = this.simulatedDataService.getTemporaryLoans();
+    this.simulationPerformed = this.simulatedDataService.isSimulationPerformed();
     // this.pushTemporaryLoans();
   }
 
 
-  pushTemporaryLoans() {
-    // if (this.temporaryLoans) {
-    //   this.temporaryLoans.forEach((loan) => {
-    //     this.dataSource.data.push(loan);
-    //     console.log(this.dataSource.data);
-    //     console.log(this.temporaryLoans);
-    //   });
-    // }
-    
-  }
 
-//  updateTableData(): void {
-//     this.get.getPNData((PNData) => {
-//         if (PNData) {
-//             // Filter out entries without a loan_no
-//             const filteredPNData = PNData.filter(entry => entry.loan_no);
-
-//             const uniqueCisNumbers = [...new Set(filteredPNData.map((entry) => entry.cis_no))];
-            
-//             uniqueCisNumbers.forEach((cisNumber) => {
-//                 HoldOutValue(cisNumber)
-//                 .then((response) => {
-//                     // Your existing logic for updating data
-//                     // Make sure to use filteredPNData instead of PNData in calculations
-//                     const holdOUT = response.result[0].Data[0].hold_out;
-
-//                     if (holdOUT) {
-//                         // Distribute hold_out data equally among PN data entries
-//                         const entries = filteredPNData.filter((entry) => entry.cis_no === cisNumber);
-//                         const holdOutValue = holdOUT || 0;
-//                         const holdOutPerCis = entries.length > 0 ? holdOutValue / entries.length : 0;
-
-//                         // Update PN Data with divided hold_out values
-//                         entries.forEach((entry) => {
-//                             entry.hold_out = Number(holdOutPerCis.toFixed(2));
-//                             this.totalHoldOut += entry.hold_out;
-//                         });
-                        
-//                         // Update the dataSource with the combined data
-//                         this.dataSource.data = filteredPNData;
-
-//                         // Calculate sums and ratios based on the filtered data
-//                         const sumPrincipal = filteredPNData.reduce((acc, obj) => {
-//                             acc.principal += parseFloat(obj.principal) || 0;
-//                             acc.principal_bal += parseFloat(obj.principal_bal) || 0;
-//                             acc.deposit_holdout += parseFloat(obj.deposit_holdout) || 0;
-//                             return acc;
-//                         }, { principal: 0, principal_bal: 0, deposit_holdout: 0 });
-                    
-//                         this.rptBal = sumPrincipal.principal_bal - sumPrincipal.deposit_holdout;
-//                         const percentage = `${((this.rptBal / this.unimpairedCap) * 100).toFixed(2)}%`;
-//                         this.rptRatio = percentage;
-//                         this.ttlRPTOL = sumPrincipal.principal;
-//                         this.ttlRPTOB = sumPrincipal.principal_bal
-//                         this.dataSource.data = filteredPNData;
-//                         this.availRptRatio = `${(this.definedRptRatio - parseFloat(percentage.replace('%', ''))).toFixed(2)}%`;
-//                         this.approvedCapital = this.unimpairedCap * 0.5;
-//                         this.availBal = this.approvedCapital - this.rptBal;
-
-//                     } else {
-//                         // Handle error or empty hold_out response
-//                     }
-//                 })
-//                 .catch((error) => {
-//                     // Handle error
-//                 });
-//             });
-//         }
-//     })
-// }
-
-updateTableData(): void {
-  this.get.getPNData((PNData) => {
-    if (PNData) {
-      const uniqueCisNumbers = [...new Set(PNData.map((entry) => entry.cis_no))];
-      
-      uniqueCisNumbers.forEach((cisNumber) => {
-        HoldOutValue(cisNumber) // Pass the entire formData object
-        .then((response) => {
-          // Log the response when the promise is resolved
-          // Handle hold_out data if needed
-          
-           // Calculate sums and ratios based on the filtered data
-           
-          this.calculateactualData(PNData);
-          
-          // Calculate simulated data
-        })
-        .catch((error) => {
-          // Handle error
+  updateTableData(): void {
+    this.get.getPNData((PNData) => {
+      if (PNData) {
+        const uniqueCisNumbers = [...new Set(PNData.map((entry) => entry.cis_no))];
+        
+        uniqueCisNumbers.forEach((cisNumber) => {
+          HoldOutValue(cisNumber)
+            .then((response) => {
+              this.calculateactualData(PNData);
+            })
+            .catch((error) => {
+              // Handle error
+            });
         });
-      });
-
-      
-    // Push temporary loan data only once outside the forEach loop
-    const tempData = PNData.slice(); // Create a shallow copy of PNData
-    if (this.temporaryLoans) {
-      tempData.push(...this.temporaryLoans); // Spread the temporary loans array to push each loan individually
-    }
-    // Update the dataSource with the combined data
-    this.dataSource.data = tempData;
-    }
-  });
-}
-
-calculateactualData(actualData: any[]): void {
-  const sumPrincipal = actualData.reduce((acc, obj) => {
-    acc.principal += parseFloat(obj.principal) || 0;
-    acc.principal_bal += parseFloat(obj.principal_bal) || 0;
-    acc.deposit_holdout += parseFloat(obj.deposit_holdout) || 0;
-    return acc;
-  }, { principal: 0, principal_bal: 0, deposit_holdout: 0 });
   
-  this.rptBal = sumPrincipal.principal_bal - sumPrincipal.deposit_holdout;
-  const percentage = `${((this.rptBal / this.unimpairedCap) * 100).toFixed(2)}%`;
-  this.rptRatio = percentage;
-  this.ttlRPTOL = sumPrincipal.principal;
-  this.ttlRPTOB = sumPrincipal.principal_bal;
-  this.availRptRatio = `${(this.definedRptRatio - parseFloat(percentage.replace('%', ''))).toFixed(2)}%`;
-  this.approvedCapital = this.unimpairedCap * 0.5;
-  this.availBal = this.approvedCapital - this.rptBal;
-}
-
-
-calculateSimulatedData(tempData: any[]): void {
-  console.log(tempData);
-  if (this.temporaryLoans) {
-    tempData.push(...this.temporaryLoans); // Spread the temporary loans array to push each loan individually
+        // Push temporary loan data only once outside the forEach loop
+        const tempData = PNData.slice(); // Create a shallow copy of PNData
+        if (this.temporaryLoans) {
+          tempData.push(...this.temporaryLoans);
+            this.calculateSimulatedData(tempData); // Spread the temporary loans array to push each loan individually
+           // Only calculate simulated data if temporary loan data is pushed
+        }
+        
+        // Update the dataSource with the combined data
+        this.dataSource.data = tempData;
+      }
+    });
   }
   
-  // Simulated data calculations go here
-  const sumPrincipal = tempData.reduce((acc, obj) => {
-    acc.principal += parseFloat(obj.principal) || 0;
-    acc.principal_bal += parseFloat(obj.principal_bal) || 0;
-    acc.deposit_holdout += parseFloat(obj.deposit_holdout) || 0;
-    return acc;
-  }, { principal: 0, principal_bal: 0, deposit_holdout: 0 });
   
-  this.SimulatedrptBal = sumPrincipal.principal_bal - sumPrincipal.deposit_holdout;
-  const percentage = `${((this.SimulatedrptBal / this.unimpairedCap) * 100).toFixed(2)}%`;
-  this.SimulatedrptRatio = percentage;
-  this.SimulatedttlRPTOL = sumPrincipal.principal;
-  this.SimulatedttlRPTOB = sumPrincipal.principal_bal;
-  this.SimulatedavailRptRatio = `${(this.definedRptRatio - parseFloat(percentage.replace('%', ''))).toFixed(2)}%`;
-  this.SimulatedapprovedCapital = this.unimpairedCap * 0.5;
-  // this.SimulatedavailBal = this.SimulatedapprovedCapital - this.SimulatedrptBal;
-  this.SimulatedavailBal = Math.max(0, this.SimulatedapprovedCapital - this.SimulatedrptBal);
 
-}
-
-//  updateTableData(): void {
-//   this.get.getPNData((PNData) => {
-//     if (PNData) {
-//       const uniqueCisNumbers = [...new Set(PNData.map((entry) => entry.cis_no))];
-      
-//       uniqueCisNumbers.forEach((cisNumber) => {
-//         HoldOutValue(cisNumber) // Pass the entire formData object
-//         .then((response) => {
-//           // Log the response when the promise is resolved
-//             // const holdOUT = response.result[0].Data[0].hold_out;
-
-//             // if (holdOUT) {
-//             //     // Distribute hold_out data equally among PN data entries
-//             //     const entries = PNData.filter((entry) => entry.cis_no === cisNumber);
-//             //     const holdOutValue = holdOUT || 0;
-//             //     const holdOutPerCis = entries.length > 0 ? holdOutValue / entries.length : 0;
-
-//             //     // Update PN Data with divided hold_out values
-//             //     entries.forEach((entry) => {
-//             //       entry.hold_out = Number(holdOutPerCis.toFixed(2));
-//             //       this.totalHoldOut += entry.hold_out;
-//             //     });
+  calculateactualData(actualData: any[]): void {
+    const sumPrincipal = actualData.reduce((acc, obj) => {
+      acc.principal += parseFloat(obj.principal) || 0;
+      acc.principal_bal += parseFloat(obj.principal_bal) || 0;
+      acc.deposit_holdout += parseFloat(obj.deposit_holdout) || 0;
+      return acc;
+    }, { principal: 0, principal_bal: 0, deposit_holdout: 0 });
+    
+    this.rptBal = sumPrincipal.principal_bal - sumPrincipal.deposit_holdout;
   
-//                 // Update the dataSource with the combined data
-                
-//                 // Calculate sums and ratios based on the filtered data
-//                 const sumPrincipal = PNData.reduce((acc, obj) => {
-//                           acc.principal += parseFloat(obj.principal) || 0;
-//                           acc.principal_bal += parseFloat(obj.principal_bal) || 0;
-//                           acc.deposit_holdout += parseFloat(obj.deposit_holdout) || 0;
-//                           return acc;
-//                         }, { principal: 0, principal_bal: 0, deposit_holdout: 0 });
-                      
-//                         this.rptBal = sumPrincipal.principal_bal - sumPrincipal.deposit_holdout;
-//                         const percentage = `${((this.rptBal / this.unimpairedCap) * 100).toFixed(2)}%`;
-//                         this.rptRatio = percentage;
-//                         // this.subtlOL = sumPrincipal.principal;
-//                         // this.subtlOB = this.rptBal;
-//                         this.ttlRPTOL = sumPrincipal.principal;
-//                         this.ttlRPTOB = sumPrincipal.principal_bal
-//                         this.dataSource.data = PNData;
-//                         this.availRptRatio = `${(this.definedRptRatio - parseFloat(percentage.replace('%', ''))).toFixed(2)}%`;
-//                         this.approvedCapital = this.unimpairedCap * 0.5;
-                        
-//                         this.availBal = this.approvedCapital - this.rptBal;
+    // Calculate rptRatio only if unimpairedCap is not zero
+    if (this.unimpairedCap !== 0) {
+      const percentage = `${((this.rptBal / this.unimpairedCap) * 100).toFixed(2)}%`;
+      this.rptRatio = percentage;
+      this.availRptRatio = `${(this.definedRptRatio - parseFloat(percentage.replace('%', ''))).toFixed(2)}%`;
+    } else {
+      this.rptRatio = 'N/A';
+      this.availRptRatio = 'N/A';
+    }
+  
+    this.ttlRPTOL = sumPrincipal.principal;
+    this.ttlRPTOB = sumPrincipal.principal_bal;
+    this.approvedCapital = this.unimpairedCap * 0.5;
+    
+    // Recalculate available balance only if approvedCapital is not zero
+    if (this.approvedCapital !== 0) {
+      this.availBal = this.approvedCapital - this.rptBal;
+    } else {
+      this.availBal = 0;
+    }
+  }
+  
+  calculateSimulatedData(tempData: any[]): void {
+    // Calculate sums and ratios based on the filtered data
+    const sumPrincipal = tempData.reduce((acc, obj) => {
+      acc.principal += parseFloat(obj.principal) || 0;
+      acc.principal_bal += parseFloat(obj.principal_bal) || 0;
+      acc.deposit_holdout += parseFloat(obj.deposit_holdout) || 0;
+      return acc;
+    }, { principal: 0, principal_bal: 0, deposit_holdout: 0 });
+  
+    // Update simulated balance
+    this.SimulatedrptBal = sumPrincipal.principal_bal - sumPrincipal.deposit_holdout;
+  
+    // Calculate ratios only if unimpairedCap is not zero
+    if (this.unimpairedCap !== 0) {
+      const percentage = `${((this.SimulatedrptBal / this.unimpairedCap) * 100).toFixed(2)}%`;
+      this.SimulatedrptRatio = percentage;
+      this.SimulatedavailRptRatio = `${(this.definedRptRatio - parseFloat(percentage.replace('%', ''))).toFixed(2)}%`;
+    } else {
+      this.SimulatedrptRatio = 'N/A';
+      this.SimulatedavailRptRatio = 'N/A';
+    }
+  
+    this.SimulatedttlRPTOL = sumPrincipal.principal;
+    this.SimulatedttlRPTOB = sumPrincipal.principal_bal;
+    this.SimulatedapprovedCapital = this.unimpairedCap * 0.5;
+    
+    // Recalculate available balance only if SimulatedapprovedCapital is not zero
+    if (this.SimulatedapprovedCapital !== 0) {
+      this.updateSimulatedBalances();
+    } else {
+      this.SimulatedavailBal = 0;
+    }
+  }
+  
+  updateSimulatedBalances(): void {
+    // Recalculate available balance after adding temporary loan data only if SimulatedrptBal is not zero
+    if (this.SimulatedrptBal !== 0) {
+      const simulatedavailBal = Math.max(0, this.SimulatedapprovedCapital - this.SimulatedrptBal);
+      console.log("SimulatedavailBal:", simulatedavailBal);
+  
+      // Update available balance
+      this.SimulatedavailBal = simulatedavailBal;
+      console.log("Updated SimulatedavailBal:", this.SimulatedavailBal);
+    } else {
+      this.SimulatedavailBal = 0;
+    }
+  }
+  
+  
 
-//               // } else {
-//                 // Handle error or empty hold_out response
-//               // }
 
-//         })
-//         .catch((error) => {
-         
-//         });
-//         if (this.temporaryLoans) {
-//           this.temporaryLoans.forEach((loan) => {
-//             PNData.push(loan);
-//             console.log(PNData);
-//             console.log(this.temporaryLoans);
-//           });
-//         }
 
-//         this.dataSource.data = PNData;
-          
-//       });
-//     }
-//   })
-// }
 
     getUnimpairedCap(): void {
       this.get.getUnimpairedCapital((unimpairedCap) => {
@@ -395,6 +295,13 @@ calculateSimulatedData(tempData: any[]): void {
           }
         },
       });
+    }
+
+
+    resetSimulation(): void {
+      this.simulationPerformed = false;   
+      this.simulatedDataService.resetSimulationPerformed();
+      this.ngOnInit();
     }
 }
 
