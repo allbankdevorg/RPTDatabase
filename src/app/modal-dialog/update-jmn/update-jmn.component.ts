a5407d33803cfa30af3a85350c537795c9ebcfff
01Junel <52649280+01Junel@users.noreply.github.com>
2024-02-13 09:53:08 +0800

    Change the name of the formfield

--- src/app/modal-dialog/update-jmn/update-jmn.component.ts
+++ src/app/modal-dialog/update-jmn/update-jmn.component.ts
@@ -158,8 +158,8 @@ export class UpdateJMNComponent {
   CISlookup() {
     const dataLookup = this.affForm.value;
   
-    if (dataLookup.com_cis_number) {
-      let cis = dataLookup.com_cis_number;
+    if (dataLookup.aff_com_cis_number) {
+      let cis = dataLookup.aff_com_cis_number;
       cisLookUP(cis)
         .then((response) => {
           if (Array.isArray(response.data)) {
@@ -197,7 +197,7 @@ export class UpdateJMNComponent {
   // Function to update form controls
   updateFormControls(accName: string) {
     this.affForm.patchValue({
-      com_account_name: accName,
+      aff_com_comp_name: accName,
       com_company_name: accName // Assuming you have company_name in the response
       // Add other form controls if needed
     });
