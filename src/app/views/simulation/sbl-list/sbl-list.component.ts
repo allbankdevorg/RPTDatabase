e126c17866dcbbf43d3671e389e2220c62e6893b
01Junel <52649280+01Junel@users.noreply.github.com>
2024-02-16 16:45:25 +0800

    remove commented code

--- src/app/views/simulation/sbl-list/sbl-list.component.ts
+++ src/app/views/simulation/sbl-list/sbl-list.component.ts
@@ -364,14 +364,4 @@ print() {
 
 
 
-
-
-  
-  // exportPDF() {
-  //   // Call exportToPDF() or openPDF() as needed
-  //   // For example, to open PDF from HTML element with id 'htmlData'
-  //   this.pdfExportService.openPDF('htmlData');
-  // }
-
-
 }
