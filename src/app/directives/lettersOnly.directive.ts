dba84c4fd48892a4a94c1dd95a490e7510435bb8
01Junel <52649280+01Junel@users.noreply.github.com>
2024-03-19 13:25:39 +0800

    Added the , to be accepted as input

--- src/app/directives/lettersOnly.directive.ts
+++ src/app/directives/lettersOnly.directive.ts
@@ -1,19 +1,49 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, ViewChild, AfterViewInit } from '@angular/core';
+import { AbstractControl, ControlContainer, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appLetters]'
+  selector: '[appLetters]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: LettersOnlyDirective,
+      multi: true
+    }
+  ]
 })
-export class LettersOnlyDirective {
-  private regex: RegExp = /^[a-zA-Z\s-.]*$/;
+export class LettersOnlyDirective implements Validator, AfterViewInit {
+  private regex: RegExp = /^[a-zA-Z\s-.,]*$/;
+  private control!: AbstractControl;
+  private previousValue: string = '';
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef, private controlContainer: ControlContainer) {}
+
+  ngAfterViewInit() {
+    this.control = this.controlContainer.control!.get(this.el.nativeElement.getAttribute('formControlName'))!;
+  }
 
   @HostListener('input', ['$event'])
   onInput(event: any) {
-    const initialValue = this.el.nativeElement.value;
-    this.el.nativeElement.value = initialValue.replace(/[^a-zA-Z\s-.]/g, '');
-    if (initialValue !== this.el.nativeElement.value) {
+    const currentValue = this.el.nativeElement.value;
+    
+    // Check if the current value matches the pattern
+    if (this.regex.test(currentValue)) {
+      this.previousValue = currentValue; // Update the previous value
+    } else {
+      // If the current value doesn't match the pattern, revert to the previous value
+      this.el.nativeElement.value = this.previousValue;
+    }
+
+    // Set the value to null if it doesn't match the pattern
+    if (currentValue !== this.el.nativeElement.value) {
+      this.control.markAsUntouched();
+      this.control.markAsPristine();
       event.stopPropagation();
     }
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    const isValid = this.regex.test(control.value);
+    return isValid ? null : { 'onlyLetters': true };
   }
 }
