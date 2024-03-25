e28c87934fc38a8ec34ddd0687c7b82b92b3e90d
01Junel <52649280+01Junel@users.noreply.github.com>
2024-03-25 13:42:02 +0800

    Added New Validation

--- src/app/modal-dialog/add-child-modal/add-child-modal.component.ts
+++ src/app/modal-dialog/add-child-modal/add-child-modal.component.ts
@@ -44,11 +44,11 @@ export class AddChildModalComponent implements OnInit {
     private auditTrailService: AuditTrailService,
     private get: FetchDataService) {
     this.affForm = this.formBuilder.group({
-      aff_com_cis_number: ['', [Validators.pattern(/\S+/)]],
-      aff_com_account_name: ['', [Validators.required, Validators.pattern(/\S+/)]],
-      aff_com_company_name: ['', [Validators.required, Validators.pattern(/\S+/)]],
+      aff_com_cis_number: ['', [Validators.pattern(/^[A-Za-z\d]+$/)]],
+      aff_com_account_name: ['', [Validators.required, Validators.pattern(/^[A-Za-z,.\s]+$/)]],
+      aff_com_company_name: ['', [Validators.required, Validators.pattern(/^[A-Za-z,.\s]+$/)]],
       managing_company: [{value: '', disabled: true}],
-      parent_company: ['', [Validators.pattern(/\S+/)]],
+      parent_company: ['', []],
       // commandControl: [''],
       });
       _dialogRef.disableClose = true;
