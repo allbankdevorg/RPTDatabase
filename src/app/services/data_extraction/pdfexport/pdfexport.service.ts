87721b5d3169cc7a1eb38a09dbaa35f66b464e19
01Junel <52649280+01Junel@users.noreply.github.com>
2024-04-04 09:58:26 +0800

    Added the export for each other related party under FINE, JMN AND PAVI

--- src/app/services/data_extraction/pdfexport/pdfexport.service.ts
+++ src/app/services/data_extraction/pdfexport/pdfexport.service.ts
@@ -274,7 +274,7 @@ generateBondsPDF(data: any[], filename: string, columnsToInclude: string[], head
     });
 
     // Calculate the width of the header text
-    const textWidth = doc.getTextWidth(`Bank Officers and Related Interest`);
+    const textWidth = doc.getTextWidth(`${headerText}`);
 
     // Calculate the x-coordinate to center the text
     const centerX = (doc.internal.pageSize.width - textWidth) / 2;
@@ -283,7 +283,7 @@ generateBondsPDF(data: any[], filename: string, columnsToInclude: string[], head
     const formattedDate = new Date(headerText).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
 
     // Add the header text with the formatted date
-    doc.text(`Bank Officers and Related Interest`, centerX, 20);
+    doc.text(`${headerText}`, centerX, 20);
 
     // Modify data to include PHP currency symbol
     const modifiedData = this.formatPDFBody(data, columnsToInclude);
@@ -446,6 +446,38 @@ generateBondsPDF(data: any[], filename: string, columnsToInclude: string[], head
 
     // Add the header text with the formatted date
     doc.text(`${headerText} Directors and Related Interest`, centerX, 20);
+
+    // Modify data to include PHP currency symbol
+    const modifiedData = this.formatPDFBody(data, columnsToInclude);
+
+    // Generate the table
+    (doc as any).autoTable({
+      startY: 25, // Adjust startY value to leave space for the header text
+      head: [columnsToInclude],
+      body: modifiedData,
+    });
+
+    doc.save(filename);
+  }
+
+
+  exportORPCompany(data: any[], filename: string, columnsToInclude: string[], headerText: string): void {
+    const doc = new jsPDF({
+        orientation: 'landscape',
+        format: 'legal'
+    });
+
+    // Calculate the width of the header text
+    const textWidth = doc.getTextWidth(`${headerText}`);
+
+    // Calculate the x-coordinate to center the text
+    const centerX = (doc.internal.pageSize.width - textWidth) / 2;
+
+    // Format the date in "MMMM d, yyyy" format
+    const formattedDate = new Date(headerText).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+
+    // Add the header text with the formatted date
+    doc.text(`${headerText}`, centerX, 20);
 
     // Modify data to include PHP currency symbol
     const modifiedData = this.formatPDFBody(data, columnsToInclude);
